db82655ca93bc27079983f8368aaaee85b99d43c
Filipi Simões <fiipzonfiirez@gmail.com>
2025-06-05 20:32:56 +0100

    a

--- principal.py
+++ principal.py
@@ -1,1 +1,9 @@
-ola
+def carregarPerguntas():
+    perguntas = []
+    with open('perguntas.txt', 'r') as arquivo:
+        for linha in arquivo:
+            perguntas.append(linha.strip())
+    return perguntas
+
+perguntas = carregarPerguntas()
+print(perguntas)
