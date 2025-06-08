b4056f76e657ce9b48427c08020a4f12fc7911e5
Filipi Simões <fiipzonfiirez@gmail.com>
2025-06-08 01:28:24 +0100

    leaderboard

--- principal.py
+++ principal.py
@@ -19,38 +19,39 @@ def quizz(nome, pontInicio, multInicio, nPerguntas):
         pontuacao = pontInicio
         i = 0
         for x  in rdmPerguntas:
-            print(f'\n\033[94m︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽︽\033[0m')
-            print(f'\033[94mJOGADOR: \033[91m {nome}')
-            print(f'\033[94mPergunta: {i+1} de {nPerguntas}\033[0m\033[92m   Acertou:{acertos}\033[0m\033[91m   Falhou:{falhas}\033[0m',end='')
-            print(f'\n\033[94mMultiplicador: {multiplicador:.2f}x\n\033[0m{arrPerguntas[x-1]}',end='')
+            print(f'\n\033[33m╔═══════════════════════════════════════════')
+            print(f'║\033[94mJOGADOR: \033[91m {nome}')
+            print(f'\033[33m║\033[94mPergunta: {i+1} de {nPerguntas}\033[0m\033[92m   Acertou:{acertos}\033[0m\033[91m   Falhou:{falhas}\033[0m',end='')
+            print(f'\n\033[33m║\033[94mMultiplicador: {multiplicador:.2f}x\n\033[33m║\033[0m{arrPerguntas[x-1]}',end='')
 
             y=0
             while y < len(arrRespostas):
                 if x == int(arrRespostas[y]):
                     for j in range(1, 5):
                         linhaResposta = arrRespostas[y + j].rstrip()
-                        print(f'{opcoes[j-1]} - {linhaResposta[:-1]}',end='\n')
+                        print(f'\033[33m║\033[0m{opcoes[j-1]} - {linhaResposta[:-1]}',end='\n')
                         if linhaResposta.endswith('#'):
                             opcaoCorreta = opcoes[j-1]
                 y+=5
+            print('',end=' ')
             if input().strip().upper() == opcaoCorreta:
-                print(f'\033[92mCORRETO + {(valorPergunta * multiplicador):.0f} 🗿\033[0m')
+                print(f'\033[33m║\033[92mCORRETO + {(valorPergunta * multiplicador):.0f} 🗿\033[0m')
                 acertos += 1
                 pontuacao += (valorPergunta * multiplicador)
                 multiplicador += multAcerto
             else:
-                print(f'\033[91mINCORRETO 🤣 {multFalha}x\033[0m\n\033[92mresposta correta: {opcaoCorreta}\033[0m')
+                print(f'\033[33m║\033[91mINCORRETO 🤣 {multFalha}x\033[0m\n\033[33m║\033[92mresposta correta: {opcaoCorreta}\033[0m')
                 falhas += 1
                 multiplicador += multFalha
                 
-            print(f'\033[94m︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾︾\033[0m\n')
+            print(f'\033[33m╚═══════════════════════════════════════════\n')
             i += 1
         print(f'\033[94mMultiplicador: {multiplicador:.2f}x\033[0m  \033[92mPONTUAÇÃO: {pontuacao:.0f}\033[0m',end='\n')
         print(f'\033[94mDe {nPerguntas} Pergunta(s)\033[0m\033[92m   Acertou:{acertos}\033[0m\033[91m   Falhou:{falhas}\033[0m',end='\n')
-        return(pontuacao, multiplicador)
+        return(pontuacao, multiplicador,acertos,falhas)
     except FileNotFoundError as e:
         print(f'erro: {e}')
-        return(pontFinal,multFinal)
+        return(pontFinal,multFinal,acertos,falhas)
     
 print(f'\nO multiplicador começa em \033[94m1.00x\033[0m após cada acerto aumenta \033[92m+0.10\033[0m a cada falha diminui \033[91m-0.20\033[0m\nO Jogador com maior pontuação vence, \033[92mBoa Sorte!\033[0m')
 jogadores = {}
@@ -67,14 +68,31 @@ except FileNotFoundError as e:
 
 for x in range(qtdJogadores):
     nome = input(f'\033[94mPlayer \033[91m{x+1}\033[94m Insira seu username: \033[0m')
-    jogadores[nome] = {'pontuacao': 0.0, 'multiplicador': 1.0}
+    jogadores[nome] = {'pontuacao': 0.0, 'multiplicador': 1.0, 'acertos': 0, 'falhas': 0}
 
 for nome in jogadores:
-    pont, mult = jogadores[nome]["pontuacao"], jogadores[nome]["multiplicador"]
-    pontFinal, multFinal = quizz(nome, pont, mult, nPerguntas)
-    jogadores[nome]["pontuacao"] = pontFinal
-    jogadores[nome]["multiplicador"] = multFinal
+    pont, mult = jogadores[nome]['pontuacao'], jogadores[nome]['multiplicador']
+    pontFinal, multFinal, acertos, falhas = quizz(nome, pont, mult, nPerguntas)
+    jogadores[nome]['pontuacao'] = pontFinal
+    jogadores[nome]['multiplicador'] = multFinal
+    jogadores[nome]['acertos'] = acertos
+    jogadores[nome]['falhas'] = falhas
 
-print("\n\033[95mRESULTADOS FINAIS:\033[0m")
-for nome, dados in jogadores.items():
-    print(f'\033[91m{nome} : \033[92m{dados["pontuacao"]:.0f} pontos\033[0m | \033[94m{dados["multiplicador"]:.2f}x\033[0m')
+jogadoresOrdenados = sorted(jogadores.items(), key=lambda item: item[1]['pontuacao'], reverse=True)
+print('\n\033[33m╔════════════════════════════════════════════════════════╗')
+print(f'║               \033[95mRANKING FINAL - {nPerguntas} Perguntas\033[33m              ║')
+print('╠════╦════════════════╦════════╦════════╦════════════════╣')
+print('║ \033[36m#\033[33m  ║ \033[36mJogador\033[33m	      ║ \033[36mPts\033[33m    ║ \033[36mMult\033[33m   ║ \033[32mCertas \033[33m/\033[33m \033[31mErros\033[33m ║')
+print('╠════╬════════════════╬════════╬════════╬════════════════╣')
+for i, (nome, dados) in enumerate(jogadoresOrdenados, start=1):
+    numJ = f' {i}'.ljust(4,' ')
+    nomeJ = nome.ljust(15)
+    pontosJ = f'{int(dados['pontuacao'])}'.rjust(6,' ')
+    multJ = f'{dados['multiplicador']:.2f}'.rjust(6)
+    acertosJ = f'{dados['acertos']} '.rjust(8)
+    falhasJ = f' {dados['falhas']}'.ljust(7)
+    print(f'║\033[36m{numJ}\033[33m║\033[36m{nomeJ} \033[33m║ \033[36m{pontosJ}\033[0m ║\033[36m{multJ}x \033[33m║\033[32m{acertosJ}\033[33m/\033[31m{falhasJ}\033[33m║')
+    if i < qtdJogadores:
+        print('╠════╬════════════════╬════════╬════════╬════════════════╣')
+    else:
+        print('╚════╩════════════════╩════════╩════════╩════════════════╝\033[0m')
